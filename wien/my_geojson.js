b0ce3777e30bfa84e6e97f9213f80381fd2847c7
sh4pefile <Paul.Tiefenbacher@student.uibk.ac.at>
2017-05-03 17:41:09 +0200

    Add files via upload

---
+++ wien/my_geojson.js
@@ -0,0 +1,22 @@
+window.my_geojson = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [16.316, 48.227],
+			},
+			"properties": {
+				"color": "black"
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [16.326, 48.227],
+			}
+		}
+	]
+};
